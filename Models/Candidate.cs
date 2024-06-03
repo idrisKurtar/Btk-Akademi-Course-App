f6b2e767140473539722a1c30033b3b4f77e3da0
idrisKurtar <kurtar_idris@outlook.com>
2024-06-03 10:30:40 +0300

    Course App Finish!

---
+++ Models/Candidate.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BtkAkademi.Models
+{
+    public class Candidate
+    {
+        [Required (ErrorMessage = "E-Mail is required.")]
+        public String? Email { get; set; } = String.Empty; 
+
+        [Required (ErrorMessage = "First Name is required.")]
+        public String? FirstName { get; set; } = String.Empty; 
+
+        [Required (ErrorMessage = "Last Name is required.")]
+        public String? LastName { get; set; } = String.Empty;         
+        public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
+        
+        public int? Age { get; set; } 
+        [Required (ErrorMessage = "Course is required.")]
+        public String? SelectedCourse { get; set; }= String.Empty; 
+        public DateTime ApplyAt { get; set; }
+
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
+
+
+        
+        
+
+        
+    }
+}
